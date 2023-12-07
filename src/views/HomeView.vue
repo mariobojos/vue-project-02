d7baecb574ed52595719996e3b3eee6def1a375a
Mario Bojos <m4riobojosd@gmail.com>
2023-12-07 17:19:57 +0800

    04 - Two fields in local storage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,24 @@
   import { ref } from 'vue'
 
   let food = ref(localStorage.getItem('food'))
+  let age = ref(localStorage.getItem('age'))
 
-  function write() {
-    localStorage.setItem('food', food.value);
+  function write(key, value) {
+    localStorage.setItem(key, value);
   }
 </script>
 
 <template>
   <main>
-    <label>What is your fave food?</label>
-    <input type="text" v-model="food" @input="write" />
+
+    <p>
+      <label>What is your fave food?</label>
+      <input type="text" v-model="food" @input="write('food', food)" />
+    </p>
+
+    <p>
+      <label>How old ar you?</label>
+      <input type="text" v-model="age" @input="write('age', age)" />
+    </p>
   </main>
 </template>
