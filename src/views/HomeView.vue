1e49be43abc5b3b6e48a49d21579f7cedc66d15f
Mario Bojos <m4riobojosd@gmail.com>
2023-12-10 06:48:21 +0800

    05 - Make the textarea support tab keydown without exiting textarea and also adding tab to the textarea value

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,26 @@ console.log(textarea); // Delay display, it will show the properties of ref text
 onMounted(() => {
     console.log(textarea.value);  // Better yet, display the HTML textarea at onMounted
     console.log(textarea.value.value);  // Display the ref's textarea value
+
+    textarea.value.addEventListener("keydown", (e) => {
+      const t = textarea.value;
+
+      // Check if tab key was pressed
+      if (e.keyCode === 9) {
+        // Get caret position
+        const val = t.value;
+        const start = t.selectionStart;
+        const end = t.selectionEnd;
+
+        // Set textarea value to: text before caret + tab + text after caret
+        t.value = val.substring(0, start) + "\t" + val.substring(end);
+
+        // Put caret at the right position again
+        t.selectionStart = t.selectionEnd = start + 1;
+
+        e.preventDefault();
+      }
+    });
   });
 </script>
 
