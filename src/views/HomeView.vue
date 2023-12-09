93a42a05a64ab018fa894e0ccb67f08d3ccf3f38
Mario Bojos <m4riobojosd@gmail.com>
2023-12-10 06:55:47 +0800

    05 -Code clean-up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,28 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 
-  let textarea = ref(null);
+const textarea = ref(null)
 
-console.log(textarea.value); // WiIll display null since textarea is not yet mounted
-console.log(textarea); // Delay display, it will show the properties of ref textarea
+function onKeyTabDown(e) {
+  const t = textarea.value
 
-onMounted(() => {
-    console.log(textarea.value);  // Better yet, display the HTML textarea at onMounted
-    console.log(textarea.value.value);  // Display the ref's textarea value
+  // Get caret position
+  const val = t.value
+  const start = t.selectionStart
+  const end = t.selectionEnd
 
-    textarea.value.addEventListener("keydown", (e) => {
-      const t = textarea.value;
+  // Set textarea value to: text before caret + tab + text after caret
+  t.value = val.substring(0, start) + '\t' + val.substring(end)
 
-      // Check if tab key was pressed
-      if (e.keyCode === 9) {
-        // Get caret position
-        const val = t.value;
-        const start = t.selectionStart;
-        const end = t.selectionEnd;
+  // Put caret at the right position again
+  t.selectionStart = t.selectionEnd = start + 1
 
-        // Set textarea value to: text before caret + tab + text after caret
-        t.value = val.substring(0, start) + "\t" + val.substring(end);
-
-        // Put caret at the right position again
-        t.selectionStart = t.selectionEnd = start + 1;
-
-        e.preventDefault();
-      }
-    });
-  });
+  e.preventDefault()
+}
 </script>
 
 <template>
   <main>
-    <textarea ref="textarea" style="width: 500px; height: 100%;">Hello, world!</textarea>
+    <textarea ref="textarea" @keydown.tab="onKeyTabDown" style="width: 500px; height: 100%;">Hello, world!</textarea>
   </main>
 </template>
