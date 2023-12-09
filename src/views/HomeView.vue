a5018c8918abf23a6390170f29cb9429d8a6aa0d
Mario Bojos <m4riobojosd@gmail.com>
2023-12-10 06:38:29 +0800

    05 - A few basics on Life Cycle with console.log

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+
+  let textarea = ref(null);
+
+console.log(textarea.value); // WiIll display null since textarea is not yet mounted
+console.log(textarea); // Delay display, it will show the properties of ref textarea
+
+onMounted(() => {
+    console.log(textarea.value);  // Better yet, display the HTML textarea at onMounted
+    console.log(textarea.value.value);  // Display the ref's textarea value
+  });
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <textarea ref="textarea" style="width: 500px; height: 100%;">Hello, world!</textarea>
   </main>
 </template>
