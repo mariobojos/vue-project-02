e7eda4f51ec75b6a68551c4bb2f91cac27d9b65d
Mario Bojos <m4riobojosd@gmail.com>
2023-12-11 09:38:35 +0800

    05 - Add a count button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@ import TabbableTextarea from '@/components/TabbableTextarea.vue';
 import { ref } from 'vue'
 
 let comment = ref('What can you say about the place?')
+
+let count = ref(0);
 </script>
 
 <template>
@@ -11,6 +13,8 @@ let comment = ref('What can you say about the place?')
       <TabbableTextarea
         v-model="comment"
         style="width: 100%; height: 400px;" />
+
+      <button @click.prevent="count++">{{ count }}</button>
     </form>
   </main>
 </template>
