289d8d8b1389655fb38c0d6f939e56c83dca1851
Mario Bojos <m4riobojosd@gmail.com>
2023-12-11 14:12:00 +0800

    07-State-Management - use Store State

---
+++ src/views/CounterView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { counter } from '@/stores/counterStore.js';
+
+
+</script>
+
+<template>
+  <main>
+    <p v-text="counter.count" />
+    <button @click="counter.increment()">Increment</button>
+  </main>
+</template>
