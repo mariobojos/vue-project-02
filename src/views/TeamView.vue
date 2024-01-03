0f8dc3df9b4e9dc3c89ffb397b26c5e7cddc9bc2
Mario Bojos <m4riobojos@gmail.com>
2024-01-03 14:13:41 +0800

    09 - Extract more components from TeamView

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,38 +1,16 @@
 <script setup>
-import team from '@/teams.json';
-import TeamMember from '@/components/Teams/TeamMember.vue';
-
-console.log(team[1]);
+import team from '@/team.json';
+import TeamHeader from '@/components/Teams/TeamHeader.vue';
+import TeamList from '@/components/Teams/TeamList.vue'
+import TeamFooter from '@/components/Teams/TeamFooter.vue'
 </script>
 
 <template>
-  <header class="flex justify-between">
-    <div>
-      <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400" :disabled="true">Add Member (0 Spots Left)</button>
-    </div>
-    <div>
-      <div class="inline-flex items-center text-3xl relative">
-        <img src="@/assets/smiley.png" alt="" class="mr-2">
-        <h3>Smiley Team</h3>
-        <div class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2">5</div>
-      </div>
-    </div>
-  </header>
+  <TeamHeader :team="team"></TeamHeader>
 
   <div class="place-self-center flex flex-col gap-y-3">
-    <table class="table-fixed border-spacing-2 border-separate">
-      <thead>
-      <th class="text-left px-6 py-2">Name</th>
-      <th class="text-left px-6 py-2">Email</th>
-      <th class="text-left px-6 py-2">Status</th>
-      </thead>
-      <tbody>
-        <TeamMember v-for="member in team" :key="member.id" :email="member.email" :name="member.name" :status="member.status" />
-      </tbody>
-    </table>
-    <p class="text-right text-gray-600 italic">There are no remaining team spots. Upgrade to add more.</p>
+    <TeamList :team="team" />
   </div>
-  <footer class="mt-12 bg-gray-100 py-4 text-center">
-    <h5 class="font-semibold text-lg">Smiley - 5 Member Team</h5>
-  </footer>
+
+  <TeamFooter :team="team" />
 </template>
