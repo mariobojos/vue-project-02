034e089f748d8a21da5f0d746bfd2b95dfb689f1
Mario Bojos <m4riobojos@gmail.com>
2024-01-14 12:32:21 +0800

    11 - Modal Version 1

---
+++ src/components/MyModal.vue
@@ -0,0 +1,35 @@
+<script setup>
+  defineProps({
+    show: Boolean
+  })
+</script>
+
+<template>
+  <div v-if="show" class="modal-mask">
+    <div class="modal-container">
+      <header>header</header>
+      <div>body</div>
+      <footer>
+        <p>footer test</p>
+        <button @click="$emit('close')">Close</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal-mask {
+  background-color: rgba(0, 0, 0, 0.6);
+  display: grid;
+  place-items: center;
+  inset: 0;
+  position: fixed;
+}
+
+.modal-container {
+  background-color: whitesmoke;
+  padding: 1rem;
+  max-width: 500px;
+  width: 80vw;
+}
+</style>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -3,6 +3,8 @@ import TeamHeader from '@/components/Teams/TeamHeader.vue';
 import TeamList from '@/components/Teams/TeamList.vue'
 import TeamFooter from '@/components/Teams/TeamFooter.vue'
 import { useTeamStore } from '@/stores/TeamStore.js';
+import MyModal from '@/components/MyModal.vue'
+import { ref } from 'vue';
 
 let team = useTeamStore();
 
@@ -12,14 +14,21 @@ team.fillUp();
 setTimeout(() => {
   team.grow(24);
 }, 2000);
+
+let showModal = ref(false);
 </script>
 
 <template>
-  <TeamHeader></TeamHeader>
+  <TeamHeader @add-member="showModal = true"></TeamHeader>
 
   <div class="place-self-center flex flex-col gap-y-3">
     <TeamList />
   </div>
 
   <TeamFooter />
+
+  <MyModal
+    :show="showModal"
+    @close="showModal = false"
+  />
 </template>
